fba27037497c114bb547d4bb865887e990749ffa
Wahyu Ramadan <wramadan122@yahoo.com>
2020-12-28 08:36:16 +0700

    try push to deploy

--- src/controllers/color.js
+++ src/controllers/color.js
@@ -3,43 +3,43 @@ const response = require('../helpers/respons')
 const Joi = require('joi')
 
 module.exports = {
-    addColor: (req, res) => {
-        const { color } = req.body
+  addColor: (req, res) => {
+    const { color } = req.body
 
-        if (color.trim()) {
-            addColorModel(color, result => {
-                if (result.affectedRows > 0) {
-                    return response(res, 'Add color success', { ...req.body }, true)
-                } else {
-                    return response(res, 'Fail add color', '', false)
-                }
-            })
+    if (color.trim()) {
+      addColorModel(color, result => {
+        if (result.affectedRows > 0) {
+          return response(res, 'Add color success', { ...req.body }, true)
         } else {
-            return response(res, 'Fill column', '', false)
+          return response(res, 'Fail add color', '', false)
         }
-    },
-
-    getColor: (req, res) => {
-        getColorModel(result => {
-            if (result) {
-                return response(res, 'List color', { result }, true)
-            } else {
-                return response(res, 'Fail get list', '', false)
-            }
-        })
-    },
-
-    deleteColor: (req, res) => {
-        const { id } = req.params
-
-        deleteColorModel(id, result => {
-            if (result.affectedRows > 0) {
-                console.log(result)
-                return response(res, `Id ${id} deleted`, '', true)
-            } else {
-                return response(res, `Id ${id} not found`, '', false)
-            }
-        })
+      })
+    } else {
+      return response(res, 'Fill column', '', false)
     }
+  },
+
+  getColor: (req, res) => {
+    getColorModel(result => {
+      if (result) {
+        return response(res, 'List color', { result }, true)
+      } else {
+        return response(res, 'Fail get list', '', false)
+      }
+    })
+  },
+
+  deleteColor: (req, res) => {
+    const { id } = req.params
+
+    deleteColorModel(id, result => {
+      if (result.affectedRows > 0) {
+        console.log(result)
+        return response(res, `Id ${id} deleted`, '', true)
+      } else {
+        return response(res, `Id ${id} not found`, '', false)
+      }
+    })
+  }
 
 }
--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -1,17 +1,18 @@
+/* eslint-disable camelcase */
 const response = require('../helpers/respons')
 const { addTransacionModel, getTransactionModel } = require('../models/transaction')
 
 module.exports = {
-    getTransaction: (req, res) => {
-        const encript = req.user.user
-        const id_user = encript.id_user
+  getTransaction: (req, res) => {
+    const encript = req.user.user
+    const id_user = encript.id_user
 
-        getTransactionModel(id_user, result => {
-            if (result.length) {
-                return response(res, 'Your transactions', { result }, true)
-            } else {
-                return response(res, 'You dont have a transaction', '', false)
-            }
-        })
-    }
+    getTransactionModel(id_user, result => {
+      if (result.length) {
+        return response(res, 'Your transactions', { result }, true)
+      } else {
+        return response(res, 'You dont have a transaction', '', false)
+      }
+    })
+  }
 }
