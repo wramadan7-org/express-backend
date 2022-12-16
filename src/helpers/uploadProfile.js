7a6063dce502843c6b89fff12ad31459386c9262
wramadan7-org <wramadan1203@gmail.com>
2022-12-16 18:31:16 +0700

    add jpeg ext

--- src/helpers/uploadProfile.js
+++ src/helpers/uploadProfile.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     const { name } = req.user.user
     const ext = file.originalname.split('.')[file.originalname.split('.').length - 1] // ambil type file (jpg/png)
     //  console.log('sassas', [file.originalname.split('.').length])
-    if (ext === 'jpg' || ext === 'png') {
+    if (ext === 'jpg' || ext === 'png' || ext === 'jpeg') {
       const filename = new Date().getTime().toString().concat(name.split(' ').join('-')).concat('.').concat(ext)
       cb(null, filename)
     } else {
